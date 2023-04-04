23dd0bd1cbc8216d2ee9ae8ae206ae1aa838d08d
katty-786 <naseerkainat202@gmail.com>
2023-04-04 11:28:58 +0500

    Authentication FrontEnd

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import React from 'react';
 import { Route, Routes, BrowserRouter, NavLink } from "react-router-dom";
 import SignUpForm from "./Pages/SignUpForm";
 import SignInForm from "./Pages/SignInForm";
