29197dee63b08d33b33310c1c8ce992b5f190bd6
katty-786 <naseerkainat202@gmail.com>
2023-04-04 11:00:01 +0500

    Authentication FrontEnd

--- src/App.js
+++ src/App.js
@@ -1,127 +1,49 @@
-import React from "react";
-import { Route, Routes, BrowserRouter } from "react-router-dom";
-import { Row, Col } from 'reactstrap';
-import { useState, useEffect } from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import MyNavbar from "./Components/Navbar/navbar.js"
-import DataForm from "./Components/Form/form.js"
-import UserCard from "./Components/Card/card.js"
-import UserLogin from "./Components/Login/login.js"
-import DisplayUser from "./Components/DisplayUser/display.js"
-import { useDispatch } from 'react-redux';
+import { useState } from "react";
+import { Route, Routes, BrowserRouter, NavLink } from "react-router-dom";
+import SignUpForm from "./Pages/SignUpForm";
+import SignInForm from "./Pages/SignInForm";
+
+import "./App.css";
 
 function App() {
-  const [userData, setUserData] = useState([]);
-  const [userToEdit, setUserToEdit] = useState(null);
-  const [user, setUser] = useState({});
+  const [activeLink, setActiveLink] = useState("/");
 
-  const dispatch = useDispatch()
-  useEffect(() => {
-    console.log("run") 
-    fetchData();
-  }, []);
-
-  // fetching users
-  function fetchData() {
-    console.log("run")
-    fetch('http://localhost:8085/users/read',{
-      method: 'GET'
-    })
-      .then(res => res.json())
-      .then(jsonData => {
-        dispatch({ type: 'setAllUsers', payload: jsonData })
-      })
-  }
-
-  // creating users
-  const handleCreateUser = (users) => {
-    fetch(`http://localhost:8085/users/create`, {
-      method: 'POST',
-      body: JSON.stringify(users),
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        dispatch({ type: 'createUsers', payload: data })
-      })
-      .catch((error) => console.error(error));
+  const handleLinkClick = (link) => {
+    setActiveLink(link);
   };
 
-  //editing users
-  useEffect(() => {
-    if (userToEdit) {
-      // directly spread all values from userToEdit to user state
-      setUser({
-        ...userToEdit,
-      });
-    }
-  }, [userToEdit]);
-
-  const updateUserData = (key, value) => {
-    // dynamically  set user data based on key ie. username, gender
-    setUser({
-      ...user,
-      [key]: value,
-    });
-  }
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log("run");
-    const newData = { ...user };
-  
-    if (userToEdit) {
-      fetch(`http://localhost:8085/users/`+ userToEdit.id, {
-        method: "PUT",
-        body: JSON.stringify(newData),
-      })
-      // console.log(userToEdit.id)
-        .then((res) => res.json())
-        .then((data) => {
-          dispatch({ type: "editUser", payload: newData });
-          setUserToEdit(null);
-        });
-    } else {
-      handleCreateUser(newData);
-      setUser({});
-    }
-    console.log("userToEdit:", userToEdit);
-    console.log("userData:", userData);
-    console.log("user:", user);
-    console.log("newData:", newData);
-  };
-       
   return (
     <BrowserRouter>
-      <MyNavbar />
+      <div className="App">
+        <div className="appAside" />
+        <div className="appForm">
+          <div className="pageSwitcher">
+            <NavLink
+              to="/sign-in"
+              activeclassname="pageSwitcherItem-active"
+              className="pageSwitcherItem"
+              onClick={() => handleLinkClick("/sign-in")}
+            >
+              Sign In
+            </NavLink>
+            <NavLink
+              to="/"
+              activeclassname="pageSwitcherItem-active"
+              className="pageSwitcherItem"
+              onClick={() => handleLinkClick("/")}
+            >
+              Sign Up
+            </NavLink>
+          </div>
 
-        <Routes>
-          <Route exact   path="/" element={<UserLogin />}/>
-          <Route
-            path="/contacts"
-            element={
-              <div className="row"  style={{padding:'15px'}}>
-                <Row>
-                <Col md={{size: 5}}>
-                <DataForm user={user} updateUserData={updateUserData} handleSubmit={handleSubmit} userToEdit={userToEdit}/>
-                </Col>
-
-                <Col md={{size: 7}}>
-                <UserCard key={user.id} user={user} setUserToEdit={setUserToEdit}/>
-                </Col>
-                </Row>
-              </div>
-            }
-          />
-          <Route path="/displayUserData" element={<div className="container">
-            <div className="row">
-          
-            <DisplayUser/>
-            </div>
-          </div>} />
-        </Routes>
-      
+          <Routes>
+          <Route exact path="/" element={<SignUpForm/>} />
+          <Route path="/sign-in" element={<SignInForm/>} />
+          </Routes>
+        </div>
+      </div>
     </BrowserRouter>
   );
 }
+
 export default App;
-           
